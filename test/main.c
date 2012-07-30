f2f5b458f948b750004704aea020f6534d9afeac
Arvid Patyupin <arvidpatyupin@iMac-Arvid.local>
2012-07-31 00:02:17 +1100

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by Arvid Patyupin on 31.07.12.
+//  Copyright (c) 2012 Arvid Patyupin. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
